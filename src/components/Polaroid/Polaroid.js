48d841113dee0028a79a033b7cb81b4f5ed4eedd
Anna <anna.rack@gmx.net>
2021-11-27 20:21:35 +0100

    add random rotation to pin

--- src/components/Polaroid/Polaroid.js
+++ src/components/Polaroid/Polaroid.js
@@ -5,6 +5,11 @@ export default function Polaroid({ data }) {
     alert(data.info);
   };
 
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  const pinRotation = `rotateZ(${getRandomArbitrary(-15, 15)}deg)`;
+
   return (
     <div onClick={showPolaroidInfo} className={styles.container}>
       <img className={styles.photo} src={`./assets/${data.img}`} alt="" />
@@ -17,7 +22,12 @@ export default function Polaroid({ data }) {
         }
         alt=""
       />
-      <img className={styles.pin} src={"./assets/Klammer.png"} alt="" />
+      <img
+        className={styles.pin}
+        src={"./assets/Klammer.png"}
+        style={{ transform: pinRotation }}
+        alt=""
+      />
       <div
         style={{ color: data.subtype === "dark" && "white" }}
         className={styles.text}
