ce827ffd19ff3e86c3f1a24428995032d7eacb95
Anna <anna.rack@gmx.net>
2021-11-28 22:27:36 +0100

    add background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,16 @@ import PaperNote from "../PaperNote/PaperNote.js";
 
 function App() {
   return (
-    <div className={styles.container}>
-      {data.map((item) =>
-        item.type === "polaroid" ? (
-          <Polaroid data={item} key={item.id} />
-        ) : (
-          <PaperNote data={item} key={item.id} />
-        )
-      )}
+    <div className={styles.bgContainer}>
+      <div className={styles.container}>
+        {data.map((item) =>
+          item.type === "polaroid" ? (
+            <Polaroid data={item} key={item.id} />
+          ) : (
+            <PaperNote data={item} key={item.id} />
+          )
+        )}
+      </div>
     </div>
   );
 }
