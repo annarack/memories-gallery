03d1dfc3a2cfaba07ba073c38cd12542d8d119e6
Anna <anna.rack@gmx.net>
2021-12-11 19:50:22 +0100

    use uuid as key

--- src/components/Pinboard/Pinboard.js
+++ src/components/Pinboard/Pinboard.js
@@ -1,6 +1,8 @@
 import styles from "./Pinboard.module.css";
 import Polaroid from "../Polaroid/Polaroid.js";
 import PaperNote from "../PaperNote/PaperNote.js";
+import { v4 as uuidv4 } from "uuid";
+//the uuid module comes default with create-react-app
 
 export default function Pinboard({ data }) {
   return (
@@ -9,9 +11,9 @@ export default function Pinboard({ data }) {
       <div className={styles.container}>
         {data.map((item) =>
           item.type === "polaroid" ? (
-            <Polaroid data={item} key={item.id} />
+            <Polaroid data={item} key={uuidv4()} />
           ) : (
-            <PaperNote data={item} key={item.id} />
+            <PaperNote data={item} key={uuidv4()} />
           )
         )}
       </div>
